d6e1ee33347329127d651fdf06acf5e9e78c99d3
ant1 <l1htar@bk.ru>
2025-08-17 23:36:08 +0300

    парсер принимает 2 и 3 слова и фильтрует

--- src/main.py
+++ src/main.py
@@ -3,19 +3,26 @@ from utils import parse_command
 if __name__ == "__main__":
 
     cmd_name: str = ''
+
+    tests = [
+        'list "john Smit" +79210008832',
+        'add bill +79210008832',
+        'delete "Albion Online"',
+        'delete Thor',
+    ]
     # while cmd_name != "exit":
     # todo: убрать после тестов
-    while cmd_name != '1':
+    for el in tests:
         # cmd_name = input("Enter command: ")
         # todo: убрать после тестов
-        cmd_name = 'list "Asik Ssort" +79210008832'
-        command_name, command_param = parse_command(cmd_name)
+        command_name, command_param = parse_command(el)
+        # command_name, command_param = parse_command(cmd_name)
         if command_name == "list":
             pass
         elif command_name == "new":
             pass
         else:
-            print("Unknown commad")
+            print("Unknown command")
         print(
             f"Command name = {command_name}, command_param = {command_param}")
 
--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,8 @@
 def parse_command(command):
+    # return 'fail', ''
+
     if '\"' in command:
-        comm, name, number = map(lambda el: el.strip(), command.split('"'))
-        return comm, (name, number)
+        comm, *name = filter(lambda el: el != "", map(lambda el: el.strip(), command.split('"')))
+        return comm, tuple(name)
     lst = command.split()
-    return lst[0] , (lst[1], lst[2])
+    return lst[0], tuple(lst[1:])
